cd865efa3157993adba46e64a844a802bd66bdf5
parkjueon <20123364@kookmin.ac.kr>
2016-10-19 01:44:53 +0900

    o

--- src/Sample.java
+++ src/Sample.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
 
 public class Sample {
 	public static void main(String[] args) {
-		System.out.println("Good");
+		Scanner in = new Scanner(System.in);
+		
+		String s1, s2;
+		
+		s1 = in.next();
+		s2 = in.next();
+		
+		System.out.println(s1);
+		System.out.println(s2);
 	}
 }
